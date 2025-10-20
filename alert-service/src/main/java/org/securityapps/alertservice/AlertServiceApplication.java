3df26fee9a6267e35f6bddea9977d4fcaa01477e
Kbrom Mehari <kibrommehari121@gmail.com>
2025-10-20 15:20:47 +0300

    entry point to run the application

---
+++ alert-service/src/main/java/org/securityapps/alertservice/AlertServiceApplication.java
@@ -0,0 +1,10 @@
+package org.securityapps.alertservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlertServiceApplication.class, args);
+    }
+}
