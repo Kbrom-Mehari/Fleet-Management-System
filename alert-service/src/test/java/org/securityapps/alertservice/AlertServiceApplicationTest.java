f136258925e74264a68e813efae555d102617887
Kbrom Mehari <kibrommehari121@gmail.com>
2025-10-20 15:21:34 +0300

    entry point for test

---
+++ alert-service/src/test/java/org/securityapps/alertservice/AlertServiceApplicationTest.java
@@ -0,0 +1,10 @@
+package org.securityapps.alertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AlertServiceApplicationTest {
+    @Test
+    void contextLoads() {}
+}
