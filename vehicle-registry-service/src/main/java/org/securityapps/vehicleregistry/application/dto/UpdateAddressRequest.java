e359305ab0c2c2d83444cb47f9ad3ba6782eb485
Kbrom Mehari <kibrommehari121@gmail.com>
2025-10-21 09:57:34 +0300

    DTO to update address

---
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/dto/UpdateAddressRequest.java
@@ -0,0 +1,8 @@
+package org.securityapps.vehicleregistry.application.dto;
+
+public class UpdateAddressRequest {
+    private String region;
+    private String city;
+    private String woreda;
+    private String kebele;
+}
