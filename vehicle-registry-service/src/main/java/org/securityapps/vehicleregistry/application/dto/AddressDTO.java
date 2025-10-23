e94c1c75b7d04fb9147b682061df2ab3859b3151
Kbrom Mehari <kibrommehari121@gmail.com>
2025-10-23 09:04:50 +0300

    address dto

---
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/dto/AddressDTO.java
@@ -0,0 +1,9 @@
+package org.securityapps.vehicleregistry.application.dto;
+
+public record AddressDTO(String kebeleIdNumber,
+                         String faydaNumber,
+                         String region,
+                         String city,
+                         String woreda,
+                         String kebele,
+                         String phoneNumber) { }
