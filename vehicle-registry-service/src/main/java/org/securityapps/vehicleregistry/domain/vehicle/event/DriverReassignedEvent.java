7bdd7b6673440ca49b8a5cfc3ea03148272b23fe
kbrom <kibrommehari121@gmail.com>
2025-10-29 09:26:04 +0300

    deleted

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/domain/vehicle/event/DriverReassignedEvent.java
+++
@@ -1,9 +0,0 @@
-package org.securityapps.vehicleregistry.domain.vehicle.event;
-
-import org.securityapps.vehicleregistry.domain.driver.DriverId;
-import org.securityapps.vehicleregistry.domain.vehicle.VehicleId;
-
-import java.time.Instant;
-
-public record DriverReassignedEvent(VehicleId vehicleId, DriverId oldDriverId, DriverId newDriverId, Instant timestamp) {
-}
