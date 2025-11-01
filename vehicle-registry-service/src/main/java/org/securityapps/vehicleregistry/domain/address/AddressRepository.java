88c435570b3819e88cd0905b528841d935135759
kbrom <kibrommehari121@gmail.com>
2025-11-01 10:48:15 +0300

    deleted

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/domain/address/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package org.securityapps.vehicleregistry.domain.address;
-
-import java.util.Optional;
-
-public interface AddressRepository {
-    Address save(Address address);
-    Optional<Address> findById(AddressId id);
-}
