8c9df452a6fc75f807abd4af84ab31535dc12419
kbrom <kibrommehari121@gmail.com>
2025-11-01 10:43:18 +0300

    deleted

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/adapter/outbound/persistence/jpa/JpaAddressRepository.java
+++
@@ -1,8 +0,0 @@
-package org.securityapps.vehicleregistry.adapter.outbound.persistence.jpa;
-
-import org.securityapps.vehicleregistry.domain.address.Address;
-import org.securityapps.vehicleregistry.domain.address.AddressId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaAddressRepository extends JpaRepository<Address, AddressId> {
-}
