c35fa1ecae0949034eaca071d5c48dec7399046d
kbrom <kibrommehari121@gmail.com>
2025-11-03 15:17:27 +0300

    changed addressId to address value object

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/dto/RegisterVehicleOwnerRequest.java
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/dto/RegisterVehicleOwnerRequest.java
@@ -1,5 +1,8 @@
 package org.securityapps.vehicleregistry.application.dto;
 
+import org.securityapps.vehicleregistry.common.valueObject.Address;
+
 public record RegisterVehicleOwnerRequest(
         String firstName,
-        String lastName) { }
+        String lastName,
+        Address address) { }
