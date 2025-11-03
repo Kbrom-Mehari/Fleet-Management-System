4c0b0a649a83eeaa4ed98102bb2fef5e3a096ed2
kbrom <kibrommehari121@gmail.com>
2025-11-03 15:13:54 +0300

    moved

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/usecase/vehicleowner/TransferVehicleOwnershipUseCase.java
+++
@@ -1,9 +0,0 @@
-package org.securityapps.vehicleregistry.application.usecase.vehicleowner;
-
-import org.securityapps.vehicleregistry.application.dto.VehicleTransferredResponse;
-import org.securityapps.vehicleregistry.domain.vehicle.VehicleId;
-import org.securityapps.vehicleregistry.domain.vehicleowner.VehicleOwnerId;
-
-public interface TransferVehicleOwnershipUseCase {
-    VehicleTransferredResponse transferVehicleOwnership(VehicleId vehicleId, VehicleOwnerId newOwner);
-}
