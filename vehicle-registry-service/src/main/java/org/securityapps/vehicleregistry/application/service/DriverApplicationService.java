8439ae9ab98da448237e35afb37976d9bf89c848
kbrom <kibrommehari121@gmail.com>
2025-11-03 15:24:47 +0300

    added update address method

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/service/DriverApplicationService.java
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/service/DriverApplicationService.java
@@ -1,14 +1,12 @@
 package org.securityapps.vehicleregistry.application.service;
 
-import lombok.RequiredArgsConstructor;
 import org.securityapps.vehicleregistry.application.dto.AddressDTO;
 import org.securityapps.vehicleregistry.application.dto.RegisterDriverRequest;
 import org.securityapps.vehicleregistry.application.dto.RegisterDriverResponse;
 import org.securityapps.vehicleregistry.application.dto.UpdateAddressRequest;
-import org.securityapps.vehicleregistry.application.usecase.address.UpdateAddressUseCase;
 import org.securityapps.vehicleregistry.application.usecase.driver.RegisterDriverUseCase;
-import org.securityapps.vehicleregistry.domain.address.Address;
-import org.securityapps.vehicleregistry.domain.address.AddressId;
+import org.securityapps.vehicleregistry.common.exception.ResourceNotFoundException;
+import org.securityapps.vehicleregistry.common.valueObject.Address;
 import org.securityapps.vehicleregistry.domain.driver.Driver;
 import org.securityapps.vehicleregistry.domain.driver.DriverId;
 import org.securityapps.vehicleregistry.domain.driver.DriverRepository;
@@ -17,22 +15,25 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-@RequiredArgsConstructor
+
 public class DriverApplicationService implements RegisterDriverUseCase {
     private final DriverRepository driverRepository;
-    private final UpdateAddressService  updateAddressService;
 
-    public void updateAddress(DriverId driverId, UpdateAddressRequest updateAddressRequest) {
-        Driver driver=driverRepository.findById(driverId).orElseThrow(()->new RuntimeException("Driver not found"));
-        updateAddressService.updateAddress(updateAddressRequest,driver.getAddressId());
-        driverRepository.save(driver);
-
+    public DriverApplicationService(DriverRepository driverRepository) {
+        this.driverRepository = driverRepository;
     }
-    @Override
-    public RegisterDriverResponse registerDriver(RegisterDriverRequest request, AddressDTO addressDTO) {
-        Address address = Address.create(AddressId.newId(),addressDTO.kebeleIdNumber(), addressDTO.faydaNumber(), addressDTO.region(), addressDTO.city(), addressDTO.woreda(), addressDTO.kebele(), addressDTO.phoneNumber());
-        Driver driver= Driver.register(DriverId.newId(), request.firstName(), request.lastName(), request.licenseNumber(), address.getId());
+
+    public void updateAddress(DriverId driverId, UpdateAddressRequest request) {
+        Driver driver=driverRepository.findById(driverId).orElseThrow(()->new ResourceNotFoundException("Driver not found"));
+        driver.updateAddress(request.region(),request.city(),request.woreda(),request.kebele(),request.phoneNumber());
         driverRepository.save(driver);
-        return new RegisterDriverResponse(driver.getId(), driver.getFirstName(), driver.getLastName(), driver.getLicenseNumber(), driver.getAddressId());
+    }
+
+    @Override
+    public RegisterDriverResponse registerDriver(RegisterDriverRequest request) {
+        Address address=new Address(request.address().getKebeleIdNumber(), request.address().getFaydaNumber(), request.address().getRegion(), request.address().getCity(), request.address().getWoreda(), request.address().getKebele(), request.address().getPhoneNumber());
+        Driver driver=Driver.create(DriverId.newId(),request.firstName(),request.lastName(), request.licenseNumber(), address);
+        driverRepository.save(driver);
+        return new RegisterDriverResponse(driver.getDriverId(), driver.getFirstName(), driver.getLastName(),driver.getLicenseNumber(),driver.getAddress());
     }
 }
