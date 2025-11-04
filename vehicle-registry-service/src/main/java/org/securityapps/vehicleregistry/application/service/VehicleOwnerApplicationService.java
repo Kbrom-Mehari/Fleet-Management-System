1048709f58efe666ff9425c00e4fe98b1b390fcd
kbrom <kibrommehari121@gmail.com>
2025-11-04 15:40:16 +0300

    added getVehicleOwner methods

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/service/VehicleOwnerApplicationService.java
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/service/VehicleOwnerApplicationService.java
@@ -1,40 +1,49 @@
 package org.securityapps.vehicleregistry.application.service;
 
-import lombok.RequiredArgsConstructor;
-import org.securityapps.vehicleregistry.application.dto.AddressDTO;
 import org.securityapps.vehicleregistry.application.dto.RegisterVehicleOwnerRequest;
 import org.securityapps.vehicleregistry.application.dto.RegisterVehicleOwnerResponse;
 import org.securityapps.vehicleregistry.application.dto.UpdateAddressRequest;
-import org.securityapps.vehicleregistry.application.usecase.address.UpdateAddressUseCase;
+import org.securityapps.vehicleregistry.application.usecase.vehicleowner.FindVehicleOwnerUseCase;
 import org.securityapps.vehicleregistry.application.usecase.vehicleowner.RegisterVehicleOwnerUseCase;
-import org.securityapps.vehicleregistry.domain.address.Address;
-import org.securityapps.vehicleregistry.domain.address.AddressId;
-import org.securityapps.vehicleregistry.domain.address.AddressRepository;
+import org.securityapps.vehicleregistry.application.usecase.vehicleowner.UpdateVehicleOwnerAddressUseCase;
+import org.securityapps.vehicleregistry.common.exception.ResourceNotFoundException;
+import org.securityapps.vehicleregistry.common.valueObject.Address;
 import org.securityapps.vehicleregistry.domain.vehicleowner.VehicleOwner;
 import org.securityapps.vehicleregistry.domain.vehicleowner.VehicleOwnerId;
 import org.securityapps.vehicleregistry.domain.vehicleowner.VehicleOwnerRepository;
-import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-@RequiredArgsConstructor
-public class VehicleOwnerApplicationService implements RegisterVehicleOwnerUseCase {
-    private final AddressRepository addressRepository;
+
+public class VehicleOwnerApplicationService implements RegisterVehicleOwnerUseCase,
+        UpdateVehicleOwnerAddressUseCase,
+        FindVehicleOwnerUseCase {
     private final VehicleOwnerRepository vehicleOwnerRepository;
-    private final UpdateAddressService updateAddressService;
+
+    public VehicleOwnerApplicationService(VehicleOwnerRepository vehicleOwnerRepository){
+        this.vehicleOwnerRepository = vehicleOwnerRepository;
+    }
 
     @Override
-    public RegisterVehicleOwnerResponse registerVehicleOwner(RegisterVehicleOwnerRequest request, AddressDTO addressDTO){
-        Address address=Address.create(AddressId.newId(), addressDTO.kebeleIdNumber(), addressDTO.faydaNumber(), addressDTO.region(), addressDTO.city(), addressDTO.woreda(), addressDTO.kebele(), addressDTO.phoneNumber());
-        VehicleOwner owner=VehicleOwner.register(VehicleOwnerId.newId(), request.firstName(), request.lastName(), address.getId());
-        return new RegisterVehicleOwnerResponse(owner.getId(), owner.getFirstName(), owner.getLastName(), owner.getAddressId());
+    public RegisterVehicleOwnerResponse registerVehicleOwner(RegisterVehicleOwnerRequest request){
+        Address address=new Address(request.address().getKebeleIdNumber(), request.address().getFaydaNumber(),request.address().getRegion(),request.address().getCity(),request.address().getWoreda(),request.address().getKebele(),request.address().getPhoneNumber());
+        VehicleOwner owner=VehicleOwner.create(VehicleOwnerId.newId(), request.firstName(), request.lastName(), address);
+        return new RegisterVehicleOwnerResponse(owner.getVehicleOwnerId(), owner.getFirstName(), owner.getLastName(), owner.getAddress());
     }
-    public void updateAddress(UpdateAddressRequest request,VehicleOwnerId vehicleOwnerId){
-        VehicleOwner vehicleOwner=vehicleOwnerRepository.findById(vehicleOwnerId).orElseThrow(()->new RuntimeException("vehicle owner not found"));
-        Address address=addressRepository.findById(vehicleOwner.getAddressId()).orElseThrow(()->new RuntimeException("address not found"));
-        updateAddressService.updateAddress(request,address.getId());
-        addressRepository.save(address);
+    @Override
+    public void updateAddress(VehicleOwnerId vehicleOwnerId,UpdateAddressRequest request){
+        VehicleOwner vehicleOwner=vehicleOwnerRepository.findById(vehicleOwnerId).orElseThrow(()->new ResourceNotFoundException("vehicle owner not found"));
+        vehicleOwner.updateAddress(request.region(), request.city(), request.woreda(), request.kebele(), request.phoneNumber());
+        vehicleOwnerRepository.save(vehicleOwner);
+    }
+    @Override
+    public VehicleOwner findVehicleOwner(VehicleOwnerId id){
+        return vehicleOwnerRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("vehicle owner not found"));
+    }
+    @Override
+    public VehicleOwner findVehicleOwnerByPhoneNumber(String phoneNumber){
+        return vehicleOwnerRepository.findVehicleOwnerByPhoneNumber(phoneNumber).orElseThrow(()->new ResourceNotFoundException("vehicle owner not found"));
     }
 }
