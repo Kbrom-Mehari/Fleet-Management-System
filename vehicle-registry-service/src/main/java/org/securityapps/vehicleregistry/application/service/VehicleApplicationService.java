41cc3b3b67c73051502994247696a6cb66f2560d
kbrom <kibrommehari121@gmail.com>
2025-11-04 15:39:40 +0300

    added @Override annotation on all methods

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/service/VehicleApplicationService.java
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/service/VehicleApplicationService.java
@@ -1,10 +1,10 @@
 package org.securityapps.vehicleregistry.application.service;
 
-import lombok.RequiredArgsConstructor;
 import org.securityapps.vehicleregistry.application.dto.*;
 import org.securityapps.vehicleregistry.application.usecase.vehicle.AssignDriverToVehicleUseCase;
 import org.securityapps.vehicleregistry.application.usecase.vehicle.RegisterVehicleUseCase;
-import org.securityapps.vehicleregistry.application.usecase.vehicleowner.TransferVehicleOwnershipUseCase;
+import org.securityapps.vehicleregistry.application.usecase.vehicle.TransferVehicleOwnershipUseCase;
+import org.securityapps.vehicleregistry.common.exception.ResourceNotFoundException;
 import org.securityapps.vehicleregistry.domain.driver.Driver;
 import org.securityapps.vehicleregistry.domain.driver.DriverId;
 import org.securityapps.vehicleregistry.domain.driver.DriverRepository;
@@ -19,32 +19,38 @@ import java.time.Instant;
 
 @Service
 @Transactional
-@RequiredArgsConstructor
+
 public class VehicleApplicationService implements
         RegisterVehicleUseCase,
         AssignDriverToVehicleUseCase,
         TransferVehicleOwnershipUseCase {
     private final VehicleRepository vehicleRepository;
     private final DriverRepository driverRepository;
+
+    public VehicleApplicationService(VehicleRepository vehicleRepository, DriverRepository driverRepository) {
+        this.vehicleRepository = vehicleRepository;
+        this.driverRepository = driverRepository;
+    }
+
     @Override
     public RegisterVehicleResponse registerVehicle(RegisterVehicleRequest request){
-        Vehicle vehicle=Vehicle.register(VehicleId.newId(),request.model(),request.plateNumber(),request.vin());
+        Vehicle vehicle=Vehicle.create(VehicleId.newId(),request.assignedDriverId(),request.vehicleOwnerId(),request.model(),request.plateNumber(),request.vin());
         vehicleRepository.save(vehicle);
-        return new RegisterVehicleResponse(vehicle.getVehicleId(),vehicle.getModelName(),vehicle.getPlateNumber(),vehicle.getVin());
+        return new RegisterVehicleResponse(vehicle.getVehicleId(),vehicle.getAssignedDriverId(),vehicle.getVehicleOwnerId(),vehicle.getModelName(),vehicle.getPlateNumber(),vehicle.getVin());
     }
     @Override
     public DriverAssignedResponse assignDriverToVehicle(VehicleId vehicleId, DriverId driverId) {
-        Vehicle vehicle=vehicleRepository.findById(vehicleId).orElseThrow(()->new RuntimeException("vehicle not found"));
-        Driver driver=driverRepository.findById(driverId).orElseThrow(()-> new RuntimeException("Driver Not Found"));
-        vehicle.assignDriver(driver.getId());
+        Vehicle vehicle=vehicleRepository.findById(vehicleId).orElseThrow(()->new ResourceNotFoundException("vehicle not found"));
+        Driver driver=driverRepository.findById(driverId).orElseThrow(()-> new ResourceNotFoundException("Driver Not Found"));
+        vehicle.assignDriver(driver.getDriverId());
         vehicleRepository.save(vehicle);
         return new DriverAssignedResponse(Instant.now(),vehicleId,driverId,vehicle.getVehicleOwnerId());
     }
     @Override
-    public VehicleTransferredResponse transferVehicleOwnership(VehicleId vehicleId, VehicleOwnerId newOwnerId) {
-        Vehicle vehicle=vehicleRepository.findById(vehicleId).orElseThrow(()->new RuntimeException("vehicle not found"));
-        vehicle.changeVehicleOwner(newOwnerId);
+    public VehicleTransferredResponse transferVehicleOwnership(VehicleId vehicleId,VehicleOwnerId newOwnerId) {
+        Vehicle vehicle=vehicleRepository.findById(vehicleId).orElseThrow(()->new ResourceNotFoundException("vehicle not found"));
         VehicleOwnerId oldOwnerId=vehicle.getVehicleOwnerId();
+        vehicle.changeVehicleOwner(newOwnerId);
         vehicleRepository.save(vehicle);
         return new VehicleTransferredResponse(vehicleId,oldOwnerId,newOwnerId,Instant.now());
     }
