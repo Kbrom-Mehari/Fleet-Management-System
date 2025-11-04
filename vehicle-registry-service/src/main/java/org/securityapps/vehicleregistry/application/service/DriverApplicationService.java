5ca0c4a59b0890d0356db20e826ce5a1eaaa353f
kbrom <kibrommehari121@gmail.com>
2025-11-04 15:38:29 +0300

    added getDriver methods

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/service/DriverApplicationService.java
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/service/DriverApplicationService.java
@@ -1,10 +1,11 @@
 package org.securityapps.vehicleregistry.application.service;
 
-import org.securityapps.vehicleregistry.application.dto.AddressDTO;
 import org.securityapps.vehicleregistry.application.dto.RegisterDriverRequest;
 import org.securityapps.vehicleregistry.application.dto.RegisterDriverResponse;
 import org.securityapps.vehicleregistry.application.dto.UpdateAddressRequest;
+import org.securityapps.vehicleregistry.application.usecase.driver.FindDriverUseCase;
 import org.securityapps.vehicleregistry.application.usecase.driver.RegisterDriverUseCase;
+import org.securityapps.vehicleregistry.application.usecase.driver.UpdateDriverAddressUseCase;
 import org.securityapps.vehicleregistry.common.exception.ResourceNotFoundException;
 import org.securityapps.vehicleregistry.common.valueObject.Address;
 import org.securityapps.vehicleregistry.domain.driver.Driver;
@@ -16,13 +17,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 
-public class DriverApplicationService implements RegisterDriverUseCase {
+public class DriverApplicationService implements RegisterDriverUseCase,
+        UpdateDriverAddressUseCase,
+        FindDriverUseCase {
     private final DriverRepository driverRepository;
 
     public DriverApplicationService(DriverRepository driverRepository) {
         this.driverRepository = driverRepository;
     }
 
+    @Override
     public void updateAddress(DriverId driverId, UpdateAddressRequest request) {
         Driver driver=driverRepository.findById(driverId).orElseThrow(()->new ResourceNotFoundException("Driver not found"));
         driver.updateAddress(request.region(),request.city(),request.woreda(),request.kebele(),request.phoneNumber());
@@ -35,5 +39,13 @@ public class DriverApplicationService implements RegisterDriverUseCase {
         Driver driver=Driver.create(DriverId.newId(),request.firstName(),request.lastName(), request.licenseNumber(), address);
         driverRepository.save(driver);
         return new RegisterDriverResponse(driver.getDriverId(), driver.getFirstName(), driver.getLastName(),driver.getLicenseNumber(),driver.getAddress());
+    }
+    @Override
+    public Driver findDriverById(DriverId driverId) {
+        return driverRepository.findById(driverId).orElseThrow(()->new ResourceNotFoundException("Driver not found"));
+    }
+    @Override
+    public Driver findDriverByPhoneNumber(String phoneNumber) {
+        return driverRepository.findByPhoneNumber(phoneNumber).orElseThrow(()->new ResourceNotFoundException("Driver not found"));
     }
 }
