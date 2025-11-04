a34de65b09e33be7b2c1936cebd5f75a3c4f2e8c
kbrom <kibrommehari121@gmail.com>
2025-11-04 15:49:14 +0300

    removed unnecessary public identifier

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/usecase/driver/RegisterDriverUseCase.java
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/application/usecase/driver/RegisterDriverUseCase.java
@@ -1,9 +1,8 @@
 package org.securityapps.vehicleregistry.application.usecase.driver;
 
-import org.securityapps.vehicleregistry.application.dto.AddressDTO;
 import org.securityapps.vehicleregistry.application.dto.RegisterDriverRequest;
 import org.securityapps.vehicleregistry.application.dto.RegisterDriverResponse;
 
 public interface RegisterDriverUseCase {
-  RegisterDriverResponse registerDriver(RegisterDriverRequest registerDriverRequest, AddressDTO addressDTO);
+  RegisterDriverResponse registerDriver(RegisterDriverRequest registerDriverRequest);
 }
