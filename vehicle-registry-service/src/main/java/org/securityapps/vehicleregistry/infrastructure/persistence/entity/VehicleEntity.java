a5c52002763d7711e2053b8bb13fc929c88db26e
kbrom <kibrommehari121@gmail.com>
2025-11-05 13:47:31 +0300

    moved to infrastructure directory

---
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/infrastructure/persistence/entity/VehicleEntity.java
@@ -0,0 +1,38 @@
+package org.securityapps.vehicleregistry.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name="vehicle")
+public class VehicleEntity {
+    @Id
+    @Column(name="vehicle_id")
+    private String vehicleId;
+    @Column(name = "vehicle_owner_id")
+    private String vehicleOwnerId;
+    @Column(name = "assigned_driver_id")
+    private String assignedDriverId;
+    @Column(name = "model_name")
+    private String modelName;
+    @Column(name = "plate_number")
+    private String plateNumber;
+    private String vin;
+
+    public VehicleEntity(String vehicleId, String vehicleOwnerId, String assignedDriverId, String modelName, String plateNumber, String vin) {
+        this.vehicleId = vehicleId;
+        this.vehicleOwnerId = vehicleOwnerId;
+        this.assignedDriverId = assignedDriverId;
+        this.modelName = modelName;
+        this.plateNumber = plateNumber;
+        this.vin = vin;
+    }
+    public VehicleEntity() {}
+
+    public String getVehicleId() {return vehicleId;}
+    public String getVehicleOwnerId() {return vehicleOwnerId;}
+    public String getAssignedDriverId() {return assignedDriverId;}
+    public String getModelName() {return modelName;}
+    public String getPlateNumber() {return plateNumber;}
+    public String getVin() {return vin;}
+}
