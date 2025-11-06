e69f78219da2d848adac4a9c821f3f6dc77af1cc
kbrom <kibrommehari121@gmail.com>
2025-11-06 12:46:08 +0300

    moved

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/outbound/persistence/repository/JpaDriverRepository.java
+++
@@ -1,8 +0,0 @@
-package org.securityapps.vehicleregistry.outbound.persistence.repository;
-
-import org.securityapps.vehicleregistry.outbound.persistence.entity.DriverEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaDriverRepository extends JpaRepository<DriverEntity, String> {
-
-}
