aca21d431baf162027023b005655978bef9a59c0
kbrom <kibrommehari121@gmail.com>
2025-11-06 12:46:38 +0300

    moved

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/outbound/persistence/repository/JpaVehicleOwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package org.securityapps.vehicleregistry.outbound.persistence.repository;
-
-import org.securityapps.vehicleregistry.outbound.persistence.entity.VehicleOwnerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaVehicleOwnerRepository extends JpaRepository<VehicleOwnerEntity, String> {
-}
