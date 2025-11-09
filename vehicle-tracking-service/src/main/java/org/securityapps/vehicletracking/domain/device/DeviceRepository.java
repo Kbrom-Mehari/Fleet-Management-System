bf7114d338c473d19f5644f9d2ab9a8c4303ed6f
kbrom <kibrommehari121@gmail.com>
2025-11-09 15:49:28 +0300

    deleted

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/domain/device/DeviceRepository.java
+++
@@ -1,6 +0,0 @@
-package org.securityapps.vehicletracking.domain.device;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceRepository extends JpaRepository<Device,Long> {
-}
