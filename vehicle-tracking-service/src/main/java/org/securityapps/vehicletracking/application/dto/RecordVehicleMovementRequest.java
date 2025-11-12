fe0128e2d84d95b07098c59cc581edb204143fcb
kbrom <kibrommehari121@gmail.com>
2025-11-12 08:52:04 +0300

    record vehicle movement

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/application/dto/RecordVehicleMovementRequest.java
+++
@@ -1,8 +0,0 @@
-package org.securityapps.vehicletracking.application.dto;
-
-import org.securityapps.vehicletracking.domain.trackerDevice.TrackerDeviceId;
-
-public record RecordVehicleMovementRequest(TrackerDeviceId trackerDeviceId,
-                                           String vehicleId,
-                                           TrackingPoint trackingPoint) {
-}
