b76b192481382b2e3b733cce60094d9cdd9e5b80
kbrom <kibrommehari121@gmail.com>
2025-11-14 11:34:17 +0300

    added update address domain logic

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/domain/driver/Driver.java
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/domain/driver/Driver.java
@@ -1,29 +1,33 @@
 package org.securityapps.vehicleregistry.domain.driver;
 
-import lombok.Getter;
-import org.securityapps.vehicleregistry.domain.address.AddressId;
+import org.securityapps.vehicleregistry.common.valueObject.Address;
 
 import java.util.Objects;
 
-@Getter
 public class Driver {
-    private final DriverId id;
+    private final DriverId driverId;
     private String firstName;
     private String lastName;
     private final String licenseNumber;
-    private final AddressId addressId;
+    private Address address;
 
-    public Driver(DriverId id,String firstName,String lastName,String licenseNumber,AddressId addressId) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.licenseNumber = licenseNumber;
-        this.addressId = addressId;
+    private Driver(DriverId driverId,String firstName,String lastName,String licenseNumber,Address address) {
+        this.driverId = driverId;
+        this.firstName = validate(firstName, "firstName");
+        this.lastName = validate(lastName, "lastName");
+        this.licenseNumber = validate(licenseNumber, "licenseNumber");
+        this.address = address;
     }
+    public DriverId getDriverId() {return driverId;}
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public String getLicenseNumber() {return licenseNumber;}
+    public Address getAddress() {return address;}
+
 
     //factory method
-    public static Driver register(DriverId driverId,String firstName,String lastName,String licenseNumber,AddressId addressId) {
-        return new Driver(driverId,firstName,lastName,licenseNumber,addressId);
+    public static Driver create(DriverId driverId,String firstName,String lastName,String licenseNumber,Address address) {
+        return new Driver(driverId,firstName,lastName,licenseNumber,address);
     }
     void changeFirstName(String firstName) {
         this.firstName = firstName;
@@ -31,15 +35,37 @@ public class Driver {
     void changeLastName(String lastName) {
         this.lastName = lastName;
     }
+    public void updateAddress(String region, String city, String woreda,String kebele,String phoneNumber) {
+        this.address=new Address(this.address.getKebeleIdNumber(),
+                this.address.getFaydaNumber(),
+                region!=null?region:this.address.getRegion(),
+                city!=null?city:this.address.getCity(),
+                woreda!=null?woreda:this.address.getWoreda(),
+                kebele!=null?kebele:this.address.getKebele(),
+                phoneNumber!=null?phoneNumber:this.address.getPhoneNumber());
+    }
+
+    private String validate(String value, String fieldName) {
+        if(value==null||value.trim().isEmpty()){
+            throw new IllegalArgumentException(fieldName+" cannot be null or empty");
+        }
+        return value.trim();
+    }
+
     @Override
     public boolean equals(Object object){
         if(this==object) return true;
         if(!(object instanceof Driver driver)) return false;
-        return Objects.equals(id, driver.id);
+        return Objects.equals(driverId, driver.driverId);
     }
     @Override
     public int hashCode(){
-        return Objects.hash(id);
+        return Objects.hash(driverId);
+    }
+
+    @Override
+    public String toString(){
+        return "DriverId: %s, FirstName: %s, LastName: %s, LicenseNumber: %s, Address: (%s)".formatted(driverId,firstName,lastName,licenseNumber,address.toString());
     }
 
 }
