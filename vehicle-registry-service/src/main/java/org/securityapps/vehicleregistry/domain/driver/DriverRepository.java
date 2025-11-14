10915bd0bdde285a37920e00c4879bef050101c4
kbrom <kibrommehari121@gmail.com>
2025-11-14 11:35:20 +0300

    added find by phone number method

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/domain/driver/DriverRepository.java
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/domain/driver/DriverRepository.java
@@ -4,5 +4,6 @@ import java.util.Optional;
 
 public interface DriverRepository {
     Optional<Driver> findById(DriverId id);
+    Optional<Driver> findByPhoneNumber(String phoneNumber);
     Driver save(Driver driver);
 }
