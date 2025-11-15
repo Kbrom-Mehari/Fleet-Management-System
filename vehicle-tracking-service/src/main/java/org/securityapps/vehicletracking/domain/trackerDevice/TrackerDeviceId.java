6cb551edba4771e45149096f1b55348b1e77eca0
kbrom <kibrommehari121@gmail.com>
2025-11-15 12:00:57 +0300

    fixed the newId method

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/domain/trackerDevice/TrackerDeviceId.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/domain/trackerDevice/TrackerDeviceId.java
@@ -3,6 +3,6 @@ package org.securityapps.vehicletracking.domain.trackerDevice;
 import java.util.UUID;
 
 public record TrackerDeviceId(String trackerDeviceId) {
-    public static String newId(){return UUID.randomUUID().toString();}
+    public static TrackerDeviceId newId(){return new TrackerDeviceId(UUID.randomUUID().toString());}
     public static TrackerDeviceId from(String trackerDeviceId){return new TrackerDeviceId(trackerDeviceId);}
 }
