8a5a31ea024ecc04cd76072133b750a26b6dae18
kbrom <kibrommehari121@gmail.com>
2025-11-15 11:52:02 +0300

    added redis and flyway dependencies

--- vehicle-tracking-service/build.gradle.kts
+++ vehicle-tracking-service/build.gradle.kts
@@ -9,8 +9,12 @@ repositories{
 dependencies{
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.apache.commons:commons-pool2")
+    implementation("org.flywaydb:flyway-core")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    runtimeOnly("com.h2database:h2")
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
     runtimeOnly("org.postgresql:postgresql")
 
 }
