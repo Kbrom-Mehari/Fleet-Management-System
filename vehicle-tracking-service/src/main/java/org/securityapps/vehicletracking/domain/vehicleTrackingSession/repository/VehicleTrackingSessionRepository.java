d5f1d02d19cd238c2695097934ec2f87b47df3a3
kbrom <kibrommehari121@gmail.com>
2025-11-15 12:04:41 +0300

    vehicle tracking session repo

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/domain/vehicleTrackingSession/repository/VehicleTrackingSessionRepository.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/domain/vehicleTrackingSession/repository/VehicleTrackingSessionRepository.java
@@ -1,15 +1,7 @@
 package org.securityapps.vehicletracking.domain.vehicleTrackingSession.repository;
 
-import org.securityapps.vehicletracking.domain.trackerDevice.TrackerDeviceId;
 import org.securityapps.vehicletracking.domain.vehicleTrackingSession.VehicleTrackingSession;
-
-import java.util.Optional;
 
 public interface VehicleTrackingSessionRepository {
     void save (VehicleTrackingSession vehicleTracking);
-    Optional<VehicleTrackingSession> findActiveSessionByTrackerDeviceId(TrackerDeviceId trackerDeviceId);
-
-
-
-
 }
