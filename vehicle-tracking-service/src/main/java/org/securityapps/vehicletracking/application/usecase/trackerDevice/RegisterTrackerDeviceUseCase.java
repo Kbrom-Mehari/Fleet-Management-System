0361bf695798ff7015837fb107c71d6384dd550c
kbrom <kibrommehari121@gmail.com>
2025-11-15 11:58:59 +0300

    register tracker device usecase

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/application/usecase/trackerDevice/RegisterTrackerDeviceUseCase.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/application/usecase/trackerDevice/RegisterTrackerDeviceUseCase.java
@@ -1,4 +1,8 @@
 package org.securityapps.vehicletracking.application.usecase.trackerDevice;
 
+import org.securityapps.vehicletracking.application.dto.RegisterTrackerDeviceDto;
+import org.securityapps.vehicletracking.domain.trackerDevice.TrackerDevice;
+
 public interface RegisterTrackerDeviceUseCase {
+    TrackerDevice registerTrackerDevice(RegisterTrackerDeviceDto trackerDeviceDto);
 }
