f1879365cb295f5cc5fb0b1cfbf3472c8341a2b0
kbrom <kibrommehari121@gmail.com>
2025-11-17 20:24:38 +0300

    tracker device mapper

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/infrastructure/persistence/mapper/TrackerDeviceMapper.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/infrastructure/persistence/mapper/TrackerDeviceMapper.java
@@ -1,4 +1,16 @@
 package org.securityapps.vehicletracking.infrastructure.persistence.mapper;
 
+import org.securityapps.vehicletracking.domain.trackerDevice.TrackerDevice;
+import org.securityapps.vehicletracking.infrastructure.persistence.entity.TrackerDeviceEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TrackerDeviceMapper {
+    public TrackerDevice toDomain (TrackerDeviceEntity entity) {
+        return TrackerDevice.rehydrate(entity.getTrackerDeviceId(), entity.getSerialNumber(), entity.getImei(),
+                entity.getSimNumber(), entity.isActive(), entity.isDamaged());
+    }
+    public TrackerDeviceEntity toEntity (TrackerDevice domain) {
+        return new TrackerDeviceEntity(domain.getTrackerDeviceId().toString(), domain.getSerialNumber(), domain.getImei(), domain.getSimNumber(), domain.isActive(), domain.isDamaged());
+    }
 }
