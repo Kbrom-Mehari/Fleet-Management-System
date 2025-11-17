bdd854b1ca9dba2ac8496df95eeca96ca61c7236
kbrom <kibrommehari121@gmail.com>
2025-11-17 20:22:35 +0300

    implemented tracker repo

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/infrastructure/persistence/adapter/TrackerDeviceRepositoryImpl.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/infrastructure/persistence/adapter/TrackerDeviceRepositoryImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.securityapps.vehicletracking.domain.trackerDevice.TrackerDevice;
 import org.securityapps.vehicletracking.domain.trackerDevice.TrackerDeviceId;
 import org.securityapps.vehicletracking.domain.trackerDevice.TrackerDeviceRepository;
+import org.securityapps.vehicletracking.infrastructure.persistence.mapper.TrackerDeviceMapper;
 import org.securityapps.vehicletracking.infrastructure.persistence.repository.JpaTrackerDeviceRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,19 +14,24 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class TrackerDeviceRepositoryImpl implements TrackerDeviceRepository {
+    private final TrackerDeviceMapper mapper;
     private final JpaTrackerDeviceRepository jpaTrackerDeviceRepository;
 
     @Override
     public void save(TrackerDevice trackerDevice){
+        var entity=mapper.toEntity(trackerDevice);
+        jpaTrackerDeviceRepository.save(entity);
 
     }
     @Override
     public Optional<TrackerDevice> findById(TrackerDeviceId id){
-        return Optional.empty();
+        var entity=jpaTrackerDeviceRepository.findById(id.toString()).orElseThrow(()->new RuntimeException("Device not found"));
+        return Optional.ofNullable(mapper.toDomain(entity));
     }
     @Override
     public Optional<TrackerDevice> findByImei(String imei){
-        return Optional.empty();
+        var entity=jpaTrackerDeviceRepository.findByImei(imei).orElseThrow(()->new RuntimeException("Device not found"));
+        return Optional.ofNullable(mapper.toDomain(entity));
     }
     @Override
     public List<TrackerDevice> findInactive(){
