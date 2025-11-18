34669d41c3a69e710433fd6d3a66b76f7fab1ddc
kbrom <kibrommehari121@gmail.com>
2025-11-18 09:22:16 +0300

    added find by imei method

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/infrastructure/persistence/repository/JpaTrackerDeviceRepository.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/infrastructure/persistence/repository/JpaTrackerDeviceRepository.java
@@ -4,6 +4,9 @@ import org.securityapps.vehicletracking.infrastructure.persistence.entity.Tracke
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface JpaTrackerDeviceRepository extends JpaRepository<TrackerDeviceEntity,String> {
+    Optional<TrackerDeviceEntity> findByImei(String imei);
 }
