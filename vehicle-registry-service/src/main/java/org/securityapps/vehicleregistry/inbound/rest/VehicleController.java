def5477891446bcdb11e4f425baf29763dfcc647
kbrom <kibrommehari121@gmail.com>
2025-11-19 20:01:51 +0300

    exposedd crud end points

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/inbound/rest/VehicleController.java
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/inbound/rest/VehicleController.java
@@ -1,24 +1,35 @@
 package org.securityapps.vehicleregistry.inbound.rest;
 
+import org.securityapps.vehicleregistry.application.usecase.vehicle.AssignDriverToVehicleUseCase;
+import org.securityapps.vehicleregistry.application.usecase.vehicle.RegisterVehicleUseCase;
+import org.securityapps.vehicleregistry.application.usecase.vehicle.TransferVehicleOwnershipUseCase;
 import org.securityapps.vehicleregistry.inbound.rest.dto.ApiResponse;
 import org.securityapps.vehicleregistry.application.dto.RegisterVehicleRequest;
 import org.securityapps.vehicleregistry.application.dto.RegisterVehicleResponse;
-import org.securityapps.vehicleregistry.application.service.VehicleApplicationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/vehicle")
+@RestController("/vehicles")
 public class VehicleController {
-    private final VehicleApplicationService vehicleApplicationService;
+    private final RegisterVehicleUseCase registerVehicleUseCase;
+    private final AssignDriverToVehicleUseCase assignDriverToVehicleUseCase;
+    private final TransferVehicleOwnershipUseCase transferVehicleOwnershipUseCase;
 
-    public VehicleController(VehicleApplicationService vehicleApplicationService) {
-        this.vehicleApplicationService = vehicleApplicationService;
+
+    public VehicleController(RegisterVehicleUseCase registerVehicleUseCase,
+                             AssignDriverToVehicleUseCase assignDriverToVehicleUseCase,
+                             TransferVehicleOwnershipUseCase transferVehicleOwnershipUseCase) {
+
+        this.registerVehicleUseCase = registerVehicleUseCase;
+        this.assignDriverToVehicleUseCase = assignDriverToVehicleUseCase;
+        this.transferVehicleOwnershipUseCase = transferVehicleOwnershipUseCase;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse<RegisterVehicleResponse>> registerVehicle(RegisterVehicleRequest registerVehicleRequest) {
-        RegisterVehicleResponse response=vehicleApplicationService.registerVehicle(registerVehicleRequest);
+    public ResponseEntity<ApiResponse<RegisterVehicleResponse>> registerVehicle(@RequestBody RegisterVehicleRequest request) {
+        RegisterVehicleResponse response=registerVehicleUseCase.registerVehicle(request);
         return ResponseEntity.ok(ApiResponse.success("vehicle registered successfully",response));
     }
 }
