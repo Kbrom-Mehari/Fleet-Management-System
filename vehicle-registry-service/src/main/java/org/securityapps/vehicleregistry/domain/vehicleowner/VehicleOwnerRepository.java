c87e7593adeefb7b9e777e677f7e0cd9d5aafc86
kbrom <kibrommehari121@gmail.com>
2025-11-19 19:59:22 +0300

    added findByPhoneNumber method

--- vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/domain/vehicleowner/VehicleOwnerRepository.java
+++ vehicle-registry-service/src/main/java/org/securityapps/vehicleregistry/domain/vehicleowner/VehicleOwnerRepository.java
@@ -4,5 +4,6 @@ import java.util.Optional;
 
 public interface VehicleOwnerRepository {
     Optional<VehicleOwner> findById(VehicleOwnerId id);
+    Optional<VehicleOwner> findVehicleOwnerByPhoneNumber(String phoneNumber);
     VehicleOwner save(VehicleOwner vehicleOwner);
 }
