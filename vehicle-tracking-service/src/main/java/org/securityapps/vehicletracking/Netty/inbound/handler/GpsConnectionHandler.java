ff999bdc0378c14789da729c44a0f7af2126a9a5
kbrom <kibrommehari121@gmail.com>
2025-12-09 08:56:42 +0300

    gps connection handler

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/handler/GpsConnectionHandler.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/handler/GpsConnectionHandler.java
@@ -4,6 +4,8 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.AttributeKey;
+import org.securityapps.vehicletracking.Netty.inbound.model.GpsLoginMessage;
+import org.securityapps.vehicletracking.Netty.inbound.model.GpsMessage;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -33,6 +35,37 @@ public class GpsConnectionHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // msg will be a decoded protocol message received from protocol decoder
+        // it could br login message or gps data message
+        if(msg instanceof GpsLoginMessage loginMessage){
+            handleLogin(ctx,loginMessage);
+            return; // we don't forward login message to the next handler (gps message handler)
+        }
+        if(msg instanceof GpsMessage gpsData){
+            String imei = ctx.channel().attr(IMEI).get();
 
+            if(imei == null){
+                System.out.println("[WARNING] device sent data before login! closing channel!");
+                ctx.close();
+                return;
+            }
+            ctx.fireChannelRead(gpsData);
+            return;
+        }
+        //if it is unknown, pass it downstream
+        ctx.fireChannelRead(msg);
     }
+
+    private void handleLogin (ChannelHandlerContext ctx, GpsLoginMessage msg){
+        String imei = msg.getImei();
+        // save IMEI into attribute channel
+        ctx.channel().attr(IMEI).set(imei);
+        // mark device as active
+        activeDevices.put(imei, ctx.channel());
+
+        System.out.println("[LOGIN] Device authenticated! " + imei);
+
+        //handshake is handled in the protocol decoder. we do nothing about handshake here
+    }
+
 }
