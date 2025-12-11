17b3e26a18b73ef9c6d579af02b405e12704f653
kbrom <kibrommehari121@gmail.com>
2025-12-11 05:29:01 +0300

    moved to frame decoder package

---
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/decoder/frameDecoder/GpsFrameDecoder.java
@@ -0,0 +1,121 @@
+package org.securityapps.vehicletracking.Netty.inbound.decoder.frameDecoder;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class GpsFrameDecoder extends ByteToMessageDecoder {
+
+    private static final int MAX_FRAME_LENGTH = 1024 * 3;//MAX 3 KB - PROTECTS OUR SERVER FROM HUGE PACKET ATTACKS
+    private static final int TLT_HEADER_ZEROS = 4;// Teltonica packet starts with four zeros
+    private static final int TLT_CRC16_BYTES = 4;//CRC - error check
+    private static final int TLT_LENGTH_BYTES = 4;
+
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in , List<Object> out) throws Exception{
+        // If there is no readable bytes, return
+        if(in.readableBytes() < 2){
+            return;
+        }
+
+        in.markReaderIndex(); // mark start point
+
+        if(isTeltonicaFrame(in)){
+            in.resetReaderIndex();
+            ByteBuf frame = decodeTeltonicaFrame(in);
+            if(frame != null){
+                out.add(frame);
+            }
+            return;
+        }
+
+        if(isGt06Frame(in)){
+            in.resetReaderIndex();
+            ByteBuf frame = decodeGt06Frame(in);
+            if(frame != null){
+                out.add(frame);
+            }
+            return;
+        }
+        // For unknown protocol, we have to move the readerIndex until we get proper protocol
+        in.resetReaderIndex();
+        in.skipBytes(1);
+    }
+
+
+    private boolean isTeltonicaFrame(ByteBuf in){
+        // Teltonica starts with 0x00 0x00 0x00 0x00 prefix
+        if(in.readableBytes() < TLT_HEADER_ZEROS) return false; // the first 4 bytes(prefix) are needed
+        int reader = in.readerIndex();
+        for(int i = 0; i < TLT_HEADER_ZEROS; i++){
+            if(in.getUnsignedByte(reader + i) != 0) return false;//the first 4 bytes should be 0x00
+        }
+        return true;
+    }
+    private ByteBuf decodeTeltonicaFrame(ByteBuf in){
+        if(in.readableBytes() < TLT_HEADER_ZEROS + TLT_LENGTH_BYTES) return null; //at least both are needed
+
+        int reader = in.readerIndex();
+
+        for(int i = 0; i < TLT_HEADER_ZEROS; i++){
+            if(in.getUnsignedByte(reader + i) != 0) return null; //not teltonica frame if the first 4 are not zero
+        }
+        //Data field length is the length of bytes [codec id, number of data 2]. excluding crc
+        int dataFieldLength = in.getInt(reader + TLT_HEADER_ZEROS); // length is found after the 4 bytes (zeros)
+
+        if(dataFieldLength <= 0 || dataFieldLength > MAX_FRAME_LENGTH) {
+            in.skipBytes(in.readableBytes()); // corrupted data. skip everything to avoid infinite loop and even connection closing
+            return null;
+        }
+
+        int totalPacketBytes = TLT_HEADER_ZEROS + TLT_LENGTH_BYTES + dataFieldLength + TLT_CRC16_BYTES;
+
+        if(in.readableBytes() < totalPacketBytes) {
+            return null;   //wait for more bytes
+        }
+
+        /* readSlice() method slices the totalPacketBytes from the original buffer and advance
+         the readerIndex of the original buffer by totalPacketBytes
+        */
+        return in.readSlice(totalPacketBytes);
+
+    }
+    private boolean isGt06Frame(ByteBuf in){   // concox Gt06 GPS devices
+        // Gt06 uses 0x78 0x78 or 0x79 0x79 start bytes
+        int a = in.getUnsignedByte(in.readerIndex());
+        int b = in.getUnsignedByte(in.readerIndex()+1); // the first two start bytes
+        return (a == 0x78 && b == 0x78) || (a == 0x79 && b == 0x79);
+    }
+    private ByteBuf decodeGt06Frame(ByteBuf in){
+        if(in.readableBytes() < 8) return null; // 2 start bit + 1 length + 1 protocol ... + 2 crc + 2 end bytes
+
+        int start1 = in.readUnsignedByte();
+        int start2 = in.readUnsignedByte();
+
+        if(!((start1 == 0x78 && start2 == 0x78)|| (start1 == 0x79 && start2 == 0x79))){
+            // if not Gt06, skip
+            return null;
+        }
+        int payloadLength = in.readUnsignedByte();
+
+        if(payloadLength <= 0 || payloadLength > MAX_FRAME_LENGTH){
+            in.skipBytes(in.readableBytes());
+            return null;
+        }
+        /*Gt06 full frame length is - start(2 bytes) + payload length(1 byte) +
+        payload[from protocol number up to crc/errorCheck](length bytes) +
+        stop(2 bytes) */
+
+        //full frameLength = start(2 bytes) + payloadLength(1 byte) + payload(length bytes) + stop(2 bytes)
+        int frameLength = payloadLength + 5;
+
+        if(in.readableBytes() < frameLength){
+            return null; // if it is incomplete frame, we wait until full frame arrives
+        }
+
+        return in.readRetainedSlice(frameLength); // return the full frame
+    }
+}
