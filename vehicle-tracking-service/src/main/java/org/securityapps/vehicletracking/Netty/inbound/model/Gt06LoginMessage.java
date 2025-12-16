67966e97b27438af5b620c365d060ff7a9f292ad
kbrom <kibrommehari121@gmail.com>
2025-12-16 10:10:52 +0300

    moved

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/model/Gt06LoginMessage.java
+++
@@ -1,9 +0,0 @@
-package org.securityapps.vehicletracking.Netty.inbound.model;
-
-import lombok.Getter;
-
-@Getter
-public class Gt06LoginMessage implements GpsLoginMessage {
-    private String imei;
-    private final String deviceType = "Gt06";
-}
