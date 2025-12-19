a7a204bf7e5390ca3bf9368360e0207d7d9d9677
kbrom <kibrommehari121@gmail.com>
2025-12-19 10:21:13 +0300

    build script

--- alert-service/build.gradle.kts
+++ alert-service/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
-    id("org.springframework.boot") version "3.5.6"
-    id("io.spring.dependency-management") version "1.1.7"
+    //spring boot plugin automatically imports the BOM like spring-boot-dependencies:x.x.x
+    alias(libs.plugins.spring.boot)
     java
 }
 repositories {
     mavenCentral()
 }
 dependencies {
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
