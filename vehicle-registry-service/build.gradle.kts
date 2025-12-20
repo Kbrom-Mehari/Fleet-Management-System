b28773e5292d38f5b23d526f4e78ebb036097fb5
kbrom <kibrommehari121@gmail.com>
2025-12-20 05:42:22 +0300

    updated the dependencies and plugins section to use aliases

--- vehicle-registry-service/build.gradle.kts
+++ vehicle-registry-service/build.gradle.kts
@@ -1,25 +1,19 @@
 plugins {
     // spring boot plugin
-    id("org.springframework.boot") version "3.5.6"
-    //spring dependency management plugin
-    id("io.spring.dependency-management") version "1.1.7"
+    alias(libs.plugins.spring.boot) //includes the spring boot BOM
+    alias(libs.plugins.spring.dependency.management)
     java
 }
 
-repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
-}
-
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    compileOnly("org.projectlombok:lombok:1.18.32")
-    annotationProcessor("org.projectlombok:lombok:1.18.32")
-    implementation("com.google.guava:guava:32.1.2-jre")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    runtimeOnly("org.postgresql:postgresql")
+    implementation(libs.spring.web)
+    implementation(libs.spring.data.jpa)
+    implementation(libs.spring.data.jdbc)
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    implementation(libs.guava)
+    testImplementation(libs.spring.test)
+    runtimeOnly(libs.postgresql)
 
 }
 
