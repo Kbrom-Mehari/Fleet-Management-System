3506e9239cd0d91df30be0875ebd584a193bc095
kbrom <kibrommehari121@gmail.com>
2025-12-20 05:47:48 +0300

    hanler that transfers gps messages into business logic

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/handler/GpsMessageHandler.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/handler/GpsMessageHandler.java
@@ -1,4 +1,35 @@
 package org.securityapps.vehicletracking.Netty.inbound.handler;
 
-public class GpsMessageHandler {
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+import org.securityapps.vehicletracking.Netty.attributes.ChannelAttributes;
+import org.securityapps.vehicletracking.Netty.inbound.model.GpsMessage;
+
+import java.time.Instant;
+
+@Slf4j
+public class GpsMessageHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg){
+        String imei = ctx.channel().attr(ChannelAttributes.IMEI).get();
+
+        if(!(msg instanceof GpsMessage gpsMessage)){
+            ctx.fireChannelRead(msg);
+            return;
+        }
+
+        if(imei == null){
+            log.error("[GPS] Received data without login! closing channel...");
+            return;
+        }
+        gpsMessage.setImei(imei);
+        gpsMessage.setReceivedAt(Instant.now()); //server time
+
+        // validating it
+        // sending to queue or rabbitmq, kafka etc
+
+
+    }
 }
