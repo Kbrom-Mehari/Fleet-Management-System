39fc3c6b6b2331adb7fe70aa8d86844423477c5e
kbrom <kibrommehari121@gmail.com>
2025-12-20 05:41:32 +0300

    updated the dependencies and plugins section to use aliases

--- alert-service/build.gradle.kts
+++ alert-service/build.gradle.kts
@@ -1,16 +1,14 @@
 plugins {
     //spring boot plugin automatically imports the BOM like spring-boot-dependencies:x.x.x
+    alias(libs.plugins.spring.dependency.management)
     alias(libs.plugins.spring.boot)
     java
 }
-repositories {
-    mavenCentral()
-}
 dependencies {
 
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    runtimeOnly("com.h2database:h2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation(libs.spring.web)
+    runtimeOnly(libs.postgresql)
+    testImplementation(libs.spring.test)
 }
 java{
 toolchain{
