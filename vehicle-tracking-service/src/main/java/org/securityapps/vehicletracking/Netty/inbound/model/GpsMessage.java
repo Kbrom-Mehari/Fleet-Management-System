eeb838e538bc780b255cd0c6c04c16a52defe39a
kbrom <kibrommehari121@gmail.com>
2025-12-21 05:26:48 +0300

    fixed a typo

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/model/GpsMessage.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/model/GpsMessage.java
@@ -1,19 +1,9 @@
 package org.securityapps.vehicletracking.Netty.inbound.model;
 
 import java.time.Instant;
-import java.util.Map;
 
-public record GpsMessage(
-    String imei,
-    double latitude,
-    double longitude,
-    int speedKph,
-    int altitude,
-    int angle,
-    int satellites,
-    int priority,
-    int eventId,
-    int totalIO,
-    Map<Integer,Object> ioElements,
-    Instant timestamp
-) { }
+public interface GpsMessage {
+    void setImei(String imei);
+    void setReceivedAt(Instant serverTime);
+    String getImei();
+}
