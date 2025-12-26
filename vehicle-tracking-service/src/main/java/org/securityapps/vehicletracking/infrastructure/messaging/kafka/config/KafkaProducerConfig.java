ca77936127a1e53d3ad6434bc52e1e965612ad37
kbrom <kibrommehari121@gmail.com>
2025-12-26 21:28:48 +0300

    added kafka template

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/infrastructure/messaging/kafka/config/KafkaProducerConfig.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/infrastructure/messaging/kafka/config/KafkaProducerConfig.java
@@ -1,4 +1,26 @@
 package org.securityapps.vehicletracking.infrastructure.messaging.kafka.config;
 
+import org.securityapps.vehicletracking.infrastructure.messaging.kafka.model.GpsRecordedEvent;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+
+@Configuration
 public class KafkaProducerConfig {
+    @Bean
+    ProducerFactory<String, GpsRecordedEvent> producerFactory(KafkaProperties kafkaProperties){
+       return new DefaultKafkaProducerFactory<>(
+               kafkaProperties.buildProducerProperties()
+       );
+    }
+    @Bean
+    public KafkaTemplate<String, GpsRecordedEvent> kafkaTemplate (
+            ProducerFactory<String,GpsRecordedEvent> producerFactory){ //injecting producer factory (DI)
+        return new KafkaTemplate<>(producerFactory);
+    }
 }
