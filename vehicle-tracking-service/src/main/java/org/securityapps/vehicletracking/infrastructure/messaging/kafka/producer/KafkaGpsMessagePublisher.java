f7b6732dd627e777a8210f8a241ff2b05dad6994
kbrom <kibrommehari121@gmail.com>
2025-12-27 20:52:56 +0300

    implemented kafka gps recorded event publisher class

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/infrastructure/messaging/kafka/producer/KafkaGpsMessagePublisher.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/infrastructure/messaging/kafka/producer/KafkaGpsMessagePublisher.java
@@ -1,19 +1,53 @@
 package org.securityapps.vehicletracking.infrastructure.messaging.kafka.producer;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.securityapps.vehicletracking.Netty.inbound.model.GpsMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.securityapps.vehicletracking.Netty.publisher.GpsMessagePublisher;
+import org.securityapps.vehicletracking.infrastructure.messaging.kafka.model.GpsRecordedEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 
-
+@Slf4j
 @RequiredArgsConstructor
 public class KafkaGpsMessagePublisher implements GpsMessagePublisher {
-    private final KafkaProducer<String, GpsMessage> producer;
+    private final KafkaTemplate<String, GpsRecordedEvent> kafkaTemplate;
+    private static final String TOPIC = "gps.recorded.v1";
 
-    public void publish(GpsMessage gpsMessage){
+    @Override
+    public void publish(GpsRecordedEvent event){
+        String key = event.imei();
+        kafkaTemplate
+                .send(TOPIC,key,event)
+                .whenComplete(
+                        (result, ex)->{
+                            if(ex != null){
+                                handleFailure(event,ex);
+                            }
+                            else
+                                handleSuccess(event,result);
+                        }
+                );
+
+    }
+    private void handleSuccess(GpsRecordedEvent event,
+                               SendResult<String, GpsRecordedEvent> result){
+        RecordMetadata metadata = result.getRecordMetadata();
+        log.debug(
+                "Gps event published | topic={}, partition={} offset={} imei={}",
+                metadata.topic(),
+                metadata.partition(),
+                metadata.offset(),
+                event.imei()
+        );
+
+    }
+    private void handleFailure(GpsRecordedEvent event, Throwable ex){
+        log.error(
+                "Failed to publish Gps Event: imei={}",
+                event.imei(),
+                ex
+        );
 
     }
 }
