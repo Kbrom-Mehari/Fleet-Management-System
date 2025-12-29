b3570d0ada9c4d9d2e46d7cbde8cbccae1e38583
kbrom <kibrommehari121@gmail.com>
2025-12-29 06:52:36 +0300

    changed gps messages into event and piblish them

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/handler/GpsMessageHandler.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/handler/GpsMessageHandler.java
@@ -2,14 +2,22 @@ package org.securityapps.vehicletracking.Netty.inbound.handler;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.securityapps.vehicletracking.Netty.attributes.ChannelAttributes;
 import org.securityapps.vehicletracking.Netty.inbound.model.GpsMessage;
+import org.securityapps.vehicletracking.Netty.inbound.model.GpsMessageAdapterFactory;
+import org.securityapps.vehicletracking.Netty.publisher.GpsMessagePublisher;
+import org.securityapps.vehicletracking.common.dto.NormalizedGpsMessage;
+import org.securityapps.vehicletracking.common.util.GpsMessageToEventMapper;
+import org.securityapps.vehicletracking.domain.event.GpsRecordedEvent;
 
 import java.time.Instant;
 
 @Slf4j
+@RequiredArgsConstructor
 public class GpsMessageHandler extends ChannelInboundHandlerAdapter {
+    private final GpsMessagePublisher publisher;
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg){
@@ -27,9 +35,9 @@ public class GpsMessageHandler extends ChannelInboundHandlerAdapter {
         gpsMessage.setImei(imei);
         gpsMessage.setReceivedAt(Instant.now()); //server time
 
-        // validating it
-        // sending to queue or rabbitmq, kafka etc
+        NormalizedGpsMessage normalized = GpsMessageAdapterFactory.adapt(gpsMessage);
+        GpsRecordedEvent event = GpsMessageToEventMapper.map(normalized);
 
-
+        publisher.publish(event);
     }
 }
