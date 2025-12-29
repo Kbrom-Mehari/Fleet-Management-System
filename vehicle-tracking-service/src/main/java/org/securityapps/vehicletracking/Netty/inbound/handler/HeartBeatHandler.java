af2799a67deb593d3b38033336d5035c91c6d8f0
kbrom <kibrommehari121@gmail.com>
2025-12-29 06:53:42 +0300

    centralized the channel attribute key for reusability

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/handler/HeartBeatHandler.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/inbound/handler/HeartBeatHandler.java
@@ -5,19 +5,19 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
-import io.netty.util.AttributeKey;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.securityapps.vehicletracking.Netty.attributes.ChannelAttributes;
 
 @Slf4j
 public class HeartBeatHandler extends ChannelInboundHandlerAdapter {
-    private static final AttributeKey<String> IMEI = AttributeKey.valueOf("imei");
 
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception{
         if(evt instanceof IdleStateEvent event){
             if(event.state() == IdleState.READER_IDLE){
                 Channel channel = ctx.channel();
-                String imei = channel.attr(IMEI).get();
+                String imei = channel.attr(ChannelAttributes.IMEI).get();
                 if(imei != null){
                     log.info("[HEARTBEAT] Device idle, closing connection: {}", imei);
                 }
@@ -31,8 +31,8 @@ public class HeartBeatHandler extends ChannelInboundHandlerAdapter {
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause){
-        log.error("[HEARTBEAT] Connection error for device : {} ",ctx.channel().attr(IMEI),cause);
+    public void exceptionCaught(@NonNull ChannelHandlerContext ctx, Throwable cause){
+        log.error("[HEARTBEAT] Connection error for device : {} ",ctx.channel().attr(ChannelAttributes.IMEI),cause);
         ctx.close();
     }
 }
