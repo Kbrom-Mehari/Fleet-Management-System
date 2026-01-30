fd23c300af5513f6b8c621baa687cdaa05bb470a
kbrom <kibrommehari121@gmail.com>
2026-01-30 14:34:39 +0300

    channel initializer class

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/GpsChannelInitializer.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/Netty/GpsChannelInitializer.java
@@ -1,4 +1,8 @@
 package org.securityapps.vehicletracking.Netty;
 
-public class GpsChannelInitializer {
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+public class GpsChannelInitializer extends ChannelInitializer<SocketChannel> {
+
 }
