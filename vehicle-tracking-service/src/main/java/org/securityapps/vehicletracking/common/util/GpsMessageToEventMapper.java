b0fb2e6f9ef79e0f15617ab38acaf8c76455419e
kbrom <kibrommehari121@gmail.com>
2026-02-01 20:46:38 +0300

    message to event mapper

--- vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/common/util/GpsMessageToEventMapper.java
+++ vehicle-tracking-service/src/main/java/org/securityapps/vehicletracking/common/util/GpsMessageToEventMapper.java
@@ -1,4 +1,17 @@
 package org.securityapps.vehicletracking.common.util;
 
+import org.securityapps.vehicletracking.common.dto.NormalizedGpsMessage;
+import org.securityapps.vehicletracking.domain.event.GpsRecordedEvent;
+
 public class GpsMessageToEventMapper {
+    public static GpsRecordedEvent map(NormalizedGpsMessage message){
+        return new GpsRecordedEvent(message.imei(),
+                message.latitude(),
+                message.longitude(),
+                message.latitude(),
+                message.speedKph(),
+                message.satellites(),
+                message.angle(),
+                message.);
+    }
 }
